cca2d22a788c3d6f5d3ab72ba907966a6bbc045c
cleve <cgomes@bloovo.com>
2017-07-10 00:41:14 +0400

    fixed postgress an created user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,27 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->json('languages');
+            $table->json('interests');
+            $table->string('mobile_number');
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
+            $table->dateTime('current_sign_in_at');
+            $table->dateTime('last_sign_in_at');
+            $table->ipAddress('current_sign_in_ip');
+            $table->ipAddress('last_sign_in_ip');
+            $table->string('confirmation_token');
+            $table->dateTime('confirmed_at');
+            $table->dateTime('confirmation_sent_at');
+            $table->string('profile_image');
+            $table->integer('country_id');
+            $table->integer('nationality_id');
+            $table->float('total_exprience', 8, 2);
+            $table->boolean('active');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
