74d30054d3b4efbc65ee4c1315f7ce7af9fa99ba
cleve <cgomes@bloovo.com>
2017-08-19 22:55:25 +0400

    added email template

--- resources/views/emails/subscriptions.blade.php
+++ resources/views/emails/subscriptions.blade.php
@@ -1,5 +1,97 @@
-<body>
-<div>
-    "Welcome Onboard! Thank you for your registration. You shall be among the first ones to hear from us regarding the beta program"
-</div>
+<body style="margin: 0; padding: 0 0 0 0; color: #FFFFFF; font-family: Times New Roman,Arial, sans-serif; font-size: 16px; line-height: 20px;">
+<table align="center" border="1" cellpadding="0" cellspacing="0" width="600">
+    <tr>
+        <td align="center" bgcolor="#FB653A" style="padding: 40px 0 30px 0;">
+            <table border="0" cellpadding="0" cellspacing="0" width="100%">
+                <tr>
+                    <td>
+                        <img src="http://www.kroble.com/img/image1.png" alt="Header Image" width="300px;"  style="display: block;" border="0" />
+                    </td>
+                    <td  width="100%">
+                        <span style="font-weight: 100px;font-size: 30px;">Welcome OnBoard!</span>
+                        <span style="display: block;padding: 40px 0 0 0;">We’re delighted to have you. You shall be among the first ones to hear about Kroble.com beta program launch.
+   </span>
+                        <span style="display: block;padding: 30px 0 0 0;">
+   We appreciate your patronage !
+   </span>
+                    </td>
+                </tr>
+
+                <tr bgcolor="#FFFFFF">
+                    <td  colspan="2">
+                        <table>
+                            <tr>
+                                <td>
+                                    <span style="display: block;font-weight: 100px;font-size: 30px;color: #8a8a8a;padding: 30px 10px 0 10px;">Our Guest on Social Media</span>
+                                    <span style="display: block;color: #8a8a8a;padding: 10px 10px 10px 10px;">Be our guest on Social Media for all the updates:</span>
+                                    <span>
+
+    <span style="padding: 0 0 0 10px;">
+      <a href="https://www.youtube.com/channel/UCm3tVQCfJNp--nct3_zofxQ"> <img src="http://www.kroble.com/img/youtube-icon_32x32.png"></a>
+    </span>
+
+    <span style="padding: 0 0 0 10px;">
+    <a href="https://www.facebook.com/Team.Kroble/">
+    <img   src="http://www.kroble.com/img/facebook-icon_32x32.png">
+    </a>
+    </span>
+     <span style="padding: 0 0 0 10px;">
+      <a href="https://twitter.com/kroble_com">
+        <img  src="http://www.kroble.com/img/instagram-icon_32x32.png">
+      </a>
+    </span>
+    <span style="padding: 0 0 0 10px;">
+    <a href="https://plus.google.com/107740471868865722381">
+    <img  src="http://www.kroble.com/img/googleplus-icon_32x32.png">
+    </a>
+    </span>
+    <span style="padding: 0 0 0 10px;">
+    <a href="https://www.linkedin.com/company-beta/10517390/">
+    <img  src="http://www.kroble.com/img/linkedin-icon_32x32.png">
+    </a>
+    </span>
+     <span style="padding: 0 0 0 10px;">
+      <a href="https://twitter.com/kroble_com">
+        <img   src="http://www.kroble.com/img/twitter-icon_32x32.png">
+    </a>
+    </span>
+   </span>
+                                </td>
+                                <td>
+                                    <img src="http://www.kroble.com/img/image3.png" alt="Body Image" width="220px;"  style="display: block;" border="0" />
+
+                                </td>
+                            </tr>
+
+                        </table>
+
+
+
+                    </td>
+
+                </tr>
+
+
+
+
+
+
+                <tr>
+                    <td>
+                        <img src="http://www.kroble.com/img/image2.png" alt="Footer Image" width="300px;"  style="display: block;" border="0" />
+                    </td>
+                    <td  width="100%">
+                        <span style="font-weight: 100px;font-size: 30px;">Coming Soon!</span>
+                        <span style="display: block;padding-top: 40px;">We couldn’t be more excited to have our beta program readiness for you.
+   </span>
+                        <span style="display: block;padding-top: 30px;">
+   Look out for www.kroble.com
+   </span>
+                    </td>
+
+                </tr>
+            </table>
+        </td>
+    </tr>
+</table>
 </body>
